408c38fba9b1fa00feaf6bfeaa98fcf1b40d65c0
oddster08 <ayushguptaa20@gmail.com>
2025-02-18 16:39:59 +0530

    Fix Vercel deployment for Express

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
-// Load env vars
+// Load environment variables
 dotenv.config();
 
 // Connect to database
@@ -30,16 +30,5 @@ app.get('/', (req, res) => {
   res.send('TODO API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(
-  PORT,
-  console.log(`Server running on port ${PORT}`)
-);
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  // Close server & exit process
-  server.close(() => process.exit(1));
-});
+// Export app for Vercel (DO NOT use app.listen)
+module.exports = app;
