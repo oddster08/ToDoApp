394013de8ce393512d46646eb39f506e12aa1583
oddster08 <ayushguptaa20@gmail.com>
2025-02-18 17:27:41 +0530

    last changes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
-// Load environment variables
+// Load env vars
 dotenv.config();
 
 // Connect to database
@@ -30,5 +30,16 @@ app.get('/', (req, res) => {
   res.send('TODO API is running...');
 });
 
-// Export app for Vercel (DO NOT use app.listen)
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
